be582e6bae9c6b9debb96d0c8ce17df7fb7961a4
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-09 20:18:32 +0100

    updates

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -4,7 +4,7 @@ import LogoImage from '../imgs/portfolio-logo.svg';
 import WorkImage from '../imgs/work-img.svg';
 import WorkVector from '../imgs/work-vector.svg';
 import './work.scss';
-
+import SquigglyLine from '../imgs/squiggly-line.svg';
 
 
 
@@ -20,6 +20,7 @@ export default class WorkView extends React.Component {
       <img id='work-img' src={WorkImage} alt=''/>
       <img id='work-vector' src={WorkVector} alt=''/>
       <div id='work-text'>
+      <img id='squiggly-line' src={SquigglyLine} alt=''/>
       <p>Thanks for checking out 
         my portfolio. If you wish, you 
         may contact me <span id='contact-link'>here</span> or via 
