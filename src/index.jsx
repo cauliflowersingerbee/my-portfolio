4f58cbf490b9cf21929be32b7a8568ab09619da0
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-10 10:57:13 +0100

    updated styles in home component

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import HomeView from './components/home/home';
+import HomeView from './components/home/home';
 //import AboutView from './components/about/about'
 //import WorkView from './components/work/work'
-import ContactView from './components/contact/contact'
+//import ContactView from './components/contact/contact'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContactView />
+    <HomeView />
   </React.StrictMode>,
   document.getElementById('root')
 );
