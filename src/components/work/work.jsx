7cf8aa8f7714b87242bae1dc03110c72e6ce3656
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-10 11:51:00 +0100

    added links to work page

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -13,6 +13,28 @@ import WorkVector from '../imgs/work-vector.svg';
 export default class WorkView extends React.Component {  
     
     render() {
+
+      const handleClickWebDev = () => {
+        window.open('_blank');
+      };
+
+      const handleClickUXDesign = () => {
+        window.open('_blank');
+      };
+
+      const handleClickWriting = () => {
+        window.open('_blank');
+      };
+    
+      //creating hover effect over linkedin link
+    //can't use hover in react so you create listeners for onMouseEnter and onMouseLeave
+    const changeTextColor = (e) => {
+      e.target.style.color = '#EF4B68';
+    }
+
+    const returnTextColor = (e) => {
+      e.target.style.color = 'white'
+    }
     
       return (
       <div>
@@ -20,6 +42,11 @@ export default class WorkView extends React.Component {
       <img id='logo-img' src={LogoImage} alt=''/>
       <img id='work-vector' src={WorkVector} alt=''/>
       <img id='work-img' src={WorkImage} alt=''/>
+      <p id='work-text'>My work</p>
+      <div id='web-dev'onClick={handleClickWebDev} style={{cursor:'pointer'}} onMouseEnter={changeTextColor} onMouseLeave={returnTextColor}>Web Development</div>
+      <div id='ux-design' onClick={handleClickUXDesign} style={{cursor:'pointer'}} onMouseEnter={changeTextColor} onMouseLeave={returnTextColor}>UX Design</div>
+      <div id='writing' onClick={handleClickWriting} style={{cursor:'pointer'}} onMouseEnter={changeTextColor} onMouseLeave={returnTextColor}>Writing</div>
+
       
       </div>
       );
