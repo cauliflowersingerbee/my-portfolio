01309ff28ba37327f471620cddffcd97cdb5407e
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-10 14:07:25 +0100

    adding routing

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,6 +4,7 @@ import HomeVector from '../imgs/home-vector.svg';
 import HomeImage from '../imgs/home-img.svg';
 import './home.scss';
 import LogoImage from '../imgs/portfolio-logo.svg';
+import { Link } from "react-router-dom";
 
 
 
@@ -27,7 +28,17 @@ export default class HomeView extends React.Component {
                 </div>
                 <p>Welcome to my portfolio.</p>
             </div>
+
+        <Link to="/home">home</Link> |{" "}
+        <Link to="/about">about</Link>
+        <Link to="/work">work</Link>
+        <Link to="/contact">contact</Link>
+        <Link to="/landing page">landing page</Link>
+        
       </div>
+
+      //adding global navigation
+      
       );
     }
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import HomeView from './components/home/home';
-//import AboutView from './components/about/about'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeView from './components/home/home';
+import AboutView from './components/about/about'
 import WorkView from './components/work/work'
-//import ContactView from './components/contact/contact'
-//import LandingPage from './components/landing-page/landing-page';
+import ContactView from './components/contact/contact'
+import LandingPage from './components/landing-page/landing-page';
 
 
 
 ReactDOM.render(
+
+  //wrapping whole app in browser router to connect browser URL 
   <React.StrictMode>
-    <WorkView />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<HomeView />} >
+        <Route path="about" element={<AboutView />} />
+        <Route path="work" element={<WorkView />} />
+        <Route path="contact" element={<ContactView />} />
+    </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
