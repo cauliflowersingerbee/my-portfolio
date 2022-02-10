a724293ca626448028a2b491edeace5fd6b1c5ce
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-10 11:00:29 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HomeView from './components/home/home';
+//import HomeView from './components/home/home';
 //import AboutView from './components/about/about'
 //import WorkView from './components/work/work'
-//import ContactView from './components/contact/contact'
+import ContactView from './components/contact/contact'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeView />
+    <ContactView />
   </React.StrictMode>,
   document.getElementById('root')
 );
