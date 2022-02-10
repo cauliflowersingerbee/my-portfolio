519214806414ee258653ec844fa0f8d8bc652b9f
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-10 13:31:23 +0100

    added simple landing for incomplete pages

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import HomeView from './components/home/home';
 //import AboutView from './components/about/about'
-import WorkView from './components/work/work'
+//import WorkView from './components/work/work'
 //import ContactView from './components/contact/contact'
+import LandingPage from './components/landing-page/landing-page';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <WorkView />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
