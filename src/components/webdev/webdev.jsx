1d43b8763c1cb53b5fdf49d5b6bf985cca59b23f
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-11 23:19:02 +0100

    updates

--- src/components/webdev/webdev.jsx
+++ src/components/webdev/webdev.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Navbar from '../navbar/navbar';
 import './webdev.scss'
 import LogoImage from '../imgs/portfolio-logo.svg';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Card, ListGroup, ListGroupItem, Modal, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyFlixAngularImg from '../imgs/myflix-angular-img.png';
 
 
 export default class WebDevView extends React.Component {  
@@ -18,19 +19,49 @@ export default class WebDevView extends React.Component {
     <div className='work-cards' >
       <div>
       <Card >
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Img variant="top" src={MyFlixAngularImg} style={{ width: '35rem', height: '15rem' }}/>
   <Card.Body>
     <Card.Title>MyFlix Angular</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+    This is a single-page, responsive movie app built with Angular, with routing and several
+    interface views. The client-side is an Angular rendition of a previous project that was built 
+    with React.  It facilitates user requests and renders the response from the server-side via 
+    a number of different interface views. The app is accompanied by relevant documentation and 
+    handoff deliverables, including a kanban board containing user stories and story points.
+    <p>
+    <h6>Technical information</h6>
+    <ul>
+        <li>Written in Angular</li>
+        <li>Uses Node.js and npm package</li>
+        <li>Contains user registration and login forms</li>
+        <li>Contains user registration and login forms</li>
+        <li>Designed using Angular Material</li>
+        <li>Documentation created using Typedoc andJSDoc</li>
+ 
+    </ul>
+    </p>
+    <p>
+    <h6>User Flows and User Stories</h6>
+    <div>
+    <Modal.Dialog>
+  <Modal.Header closeButton>
+    <Modal.Title>Modal title</Modal.Title>
+  </Modal.Header>
+
+  <Modal.Body>
+    <p>Modal body text goes here.</p>
+  </Modal.Body>
+
+  <Modal.Footer>
+    <Button variant="secondary">Close</Button>
+    <Button variant="primary">Save changes</Button>
+  </Modal.Footer>
+</Modal.Dialog>
+    </div>
+    </p>
+
     </Card.Text>
   </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
   <Card.Body>
     <Card.Link href="#">Card Link</Card.Link>
     <Card.Link href="#">Another Link</Card.Link>
