9ad23028e24a663cf17de2bbb07cc793dece8eac
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-15 12:32:36 +0100

    Update webdev.jsx

--- src/components/webdev/webdev.jsx
+++ src/components/webdev/webdev.jsx
@@ -133,11 +133,19 @@ export default class WebDevView extends React.Component {
                               <p style={{fontWeight: 'bold', color: '#000', fontSize: '20px', fontFamily: 'Roboto-Slab', lineHeight: '1px'}}>About the App</p>
                             
                             <div>
-                            <img alt='' src={MyFlixReactImg} style={{ width: '40rem', height: 'auto', float: 'right', margin: '1rem', border: 'solid 4px #fff'}}/>
+                            <img alt='' src={MyFlixReactImg} style={{ width: '40rem', height: 'auto', float: 'right', margin: '1rem', border: 'solid 8px #fff'}}/>
                      
                             
-                              <p style={{marginTop: '1rem', color: '#000', fontSize: '16px', fontFamily: 'Roboto-Slab',}}>This is the client-side of a movie API. It's built using the MERN tech stack (MongoDB, Express, React, and Node.js). It’s a single-page application (SPA). It uses routing to navigate between views and share URLs. It uses Parcel as its build tool.
-The user interface is made up of a number of views e.g. the main view (returns a list of all movies to the user), the  single movie view (returns data about a single movie to the user and allows users to add a movie to their list of favorites), the login view (allows users to log in with a username and password), the registration view (allows new users to register) and the profile view (allows users to update their user info, deregister, access and edit their list of favorite movies.
+                              <p style={{marginTop: '1rem', color: '#000', fontSize: '16px', fontFamily: 'Roboto-Slab',}}>
+                                This is the client-side of a movie API. It's built using the MERN tech stack 
+                                (MongoDB, Express, React, and Node.js). It’s a single-page application (SPA). 
+                                It uses routing to navigate between views and share URLs. It uses Parcel as its build tool.
+                                The user interface is made up of a number of views e.g. the main view (returns a list of all 
+                                movies to the user), the  single movie view (returns data about a single movie to the user 
+                                and allows users to add a movie to their list of favorites), the login view (allows users to 
+                                log in with a username and password), the registration view (allows new users to register) and 
+                                the profile view (allows users to update their user info, deregister, access and edit their 
+                                list of favorite movies).
  </p>
 
                             <Row>
@@ -152,52 +160,6 @@ The user interface is made up of a number of views e.g. the main view (returns a
                       </Col>
                  
                      
-
-                </Row>
-              
-               
-          </Card> 
-          
-          
-      </div>
-
-
-      <div id='titbit' >
-           <Card style={{background: '#F2CA52', width: '70rem', height: '40rem', marginTop: '2rem'}}>
-            <Card.Title style={{margin: '2rem', marginLeft: '27rem', lineHeight: '2.2rem', color: '#fff', fontSize: '30px', fontFamily: 'Roboto', fontWeight: '800'}}>
-                Titbit
-            </Card.Title>
-                <Row>
-                        <Col style={{margin: '2rem', marginRight: '0.5rem'}}>
-          
-                              <p style={{fontWeight: 'bold', color: '#000', fontSize: '20px', fontFamily: 'Roboto-Slab', lineHeight: '1px'}}>About the App</p>
-                            
-                            <div>
-                            <img alt='' src={TitbitImg} style={{ width: '15rem', height: 'auto', float: 'left', margin: '1rem', border: 'solid 4px #fff'}}/>
-                              
-                            
-                              <p style={{marginTop: '1rem', color: '#000', fontSize: '16px', fontFamily: 'Roboto-Slab',}}>This is a single-page, responsive movie app built with Angular, with routing and several
-                            interface views. The client-side is an Angular rendition of a previous project that I built 
-                            with React. It is designed using Angular Material, with documentation created using Typedoc and JSDoc </p>
-
-                            <Row>
-                    <Col id='site-links' style={{border:'2px solid #fff', margin:'1px'}}><Card.Link href="https://github.com/cauliflowersingerbee/titbit" target="_blank">Github</Card.Link></Col>
-                    <Col id='site-links' style={{border:'2px solid #fff', margin:'1px'}}><Card.Link href="https://cauliflowersingerbee.github.io/myFlix-Angular-client/welcome" target="_blank">Case Study</Card.Link></Col>
-                    <Col></Col>
-                   <Col></Col>
-                 
-                </Row>
-                            </div>      
-          
-                      </Col>
-                 
-                      <Col style={{marginRight:'2rem'}}>
-                      <Card style={{margin: '2rem'}}>
-                      <img id='user-flows' src={UserFlows} alt='' style={{margin: '.1rem', width: '600px',}}/>
-                      </Card>   
-                      <Row><p style={{color: '#000', fontSize: '16px', fontFamily: 'Roboto-Slab'}}>I created user stories and user flows on Adobe Illustrator. These were crucial in guiding me as I added features to the app. To keep track of the project, I used Kanban boards built with Trello. </p></Row>
-                     
-                      </Col>
 
                 </Row>
               
@@ -252,6 +214,58 @@ The user interface is made up of a number of views e.g. the main view (returns a
           
       </div>
 
+
+      <div id='titbit' >
+           <Card style={{background: '#F2CA52', width: '70rem', height: '40rem', marginTop: '2rem'}}>
+            <Card.Title style={{margin: '2rem', marginLeft: '27rem', lineHeight: '2.2rem', color: '#fff', fontSize: '30px', fontFamily: 'Roboto', fontWeight: '800'}}>
+                Titbit
+            </Card.Title>
+                <Row>
+                        <Col style={{margin: '2rem', marginRight: '0.5rem'}}>
+          
+                              <p style={{fontWeight: 'bold', color: '#000', fontSize: '20px', fontFamily: 'Roboto-Slab', lineHeight: '1px'}}>About the App</p>
+                            
+                            <div>
+                               
+                            
+                              <p style={{marginTop: '1rem', color: '#000', fontSize: '16px', fontFamily: 'Roboto-Slab',}}>Titibit is a chat app for mobile 
+                              devices, built using React Native. The app provides users with a chat interface and options to share images and their location.
+                              The app is written in React Native. It's developed using Expo. Conversations are stored in Google Firestore Database.
+                              The app authenticates users anonymously via Google Firebase authentication.
+                              Chat conversations are stored locally. The app lets users pick and send images from the phone’s image library.
+                              It lets users take and send pictures using their device’s camera app.
+                              It stores images in Firebase Cloud Storage.
+                              The app is able to read the user’s location data.
+                              Location data is sent via the chat in a map view.
+                              The chat interface and functionality are created using the Gifted Chat library.
+                            
+                              </p>
+
+                            <Row>
+                    <Col id='site-links' style={{border:'2px solid #fff', margin:'1px'}}><Card.Link href="https://github.com/cauliflowersingerbee/titbit" target="_blank">Github</Card.Link></Col>
+                    <Col id='site-links' style={{border:'2px solid #fff', margin:'1px'}}><Card.Link href="https://cauliflowersingerbee.github.io/myFlix-Angular-client/welcome" target="_blank">Case Study</Card.Link></Col>
+                    <Col></Col>
+                   <Col></Col>
+                 
+                </Row>
+                            </div>      
+          
+                      </Col>
+                 
+                      <Col style={{marginRight:'2rem'}}>
+                      <Card style={{margin: '2rem', width: '22rem'}}>
+                      <img alt='' src={TitbitImg} style={{ width: '21.5rem', height: 'auto', float: 'left', margin: '.2rem', border: 'solid 2px #fff'}}/>
+                      </Card>   
+                     
+                      </Col>
+
+                </Row>
+              
+               
+          </Card> 
+          
+          
+      </div>
 
       <div id='aptly' >
            <Card style={{background: '#F2CA52', width: '70rem', height: '40rem', marginTop: '2rem'}}>
