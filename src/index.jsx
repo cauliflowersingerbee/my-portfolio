f0e3bfff62f926762d0ddba21c25ecb32e7cc257
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-16 20:35:25 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,7 @@ ReactDOM.render(
 
   //wrapping whole app in browser router to connect browser URL 
   <React.StrictMode>
-    <BrowserRouter basename="/my-portfolio">
+    <BrowserRouter >
     <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="about" element={<AboutView />} />
