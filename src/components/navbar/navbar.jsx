a3eaf617be7782879de5d06bcb89b4f4e112b8be
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-19 12:59:04 +0100

    finally fixed navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, Container, Nav, } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './navbar.scss';
 import LogoImage from '../imgs/portfolio-logo.svg';
 
@@ -7,28 +7,27 @@ import LogoImage from '../imgs/portfolio-logo.svg';
 export default class NavbarView extends React.Component {
     render() {
         return(
-
-<Navbar collapseOnSelect expand="lg" fixed='top'>
-  <Container >
+<>
+<Navbar expand="lg" fixed='top'>
+  <Container>
   <Navbar.Brand href="#home">
-      <img id='logo-img' src={LogoImage} alt=''  className="d-inline-block align-top" style={{width: '8rem', height:'auto'}}/>
-      </Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto my-2 my-lg-0 nav-link">
-         
-          <Nav.Link href="/" id='home-link'>home</Nav.Link>
+      <img id='logo-img' src={LogoImage} alt=''  className="d-inline-block align-top" style={{width: '9rem', height:'auto'}}/>
+  </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="me-auto">
+      <Nav.Link href="/my-portfolio/home" id='home-link'>home</Nav.Link>
           <Nav.Link href="/my-portfolio/about" id='about-link'>about</Nav.Link>
           <Nav.Link href="/my-portfolio/webdevelopment" id='webdev-link'>web development</Nav.Link>
           <Nav.Link href="/my-portfolio/uxdesign" id='ux-link'>ux design</Nav.Link>
           <Nav.Link href="/my-portfolio/writing" id='writing-link'>writing</Nav.Link>
           <Nav.Link href="/my-portfolio/contact" id='contact-link'>contact</Nav.Link>
-          
-        
+  
       </Nav>
-      </Navbar.Collapse>
+    </Navbar.Collapse>
   </Container>
 </Navbar>
+</>
         );
     };
 
