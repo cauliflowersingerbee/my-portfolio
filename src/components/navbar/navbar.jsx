cf4bda600e8a9ddddb6669f5c340d538d1d7771c
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-20 15:27:47 +0100

    home page responsiveness

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,17 +11,17 @@ export default class NavbarView extends React.Component {
 <Navbar expand="lg" fixed='top' className='justify-content-between'>
   <Container>
   <Navbar.Brand href="/">
-      <img id='logo-img' src={LogoImage} alt=''  className="d-inline-block align-top" style={{width: '15%', height:'auto', marginRight: '150px', margin:'10px'}}/>
+      <img id='logo-img' src={LogoImage} alt=''  className="d-inline-block align-top" style={{width: '10%', height:'auto', margin:'10px'}}/>
   </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
       <Nav className="me-auto navbar-nav ms-auto" style={{float: 'right'}}>
-          <Nav.Link href="/" id='home-link' style={{ marginRight: '60px', color: '#F2CA52', fontWeight: 'bold', textTransform: 'capitalize', fontFamily: 'Roboto-Slab', fontSize: '12px', lineHeight: '1px' }}>HOME</Nav.Link>
-          <Nav.Link href="/my-portfolio/about" id='about-link' style={{marginRight: '60px', color: '#F2CA52', fontWeight: 'bold', textTransform: 'capitalize', fontFamily: 'Roboto-Slab', fontSize: '12px', lineHeight: '1px' }}>ABOUT</Nav.Link>
-          <Nav.Link href="/my-portfolio/webdevelopment" id='webdev-link' style={{marginRight: '50px', color: '#F2CA52', fontWeight: 'bold', textTransform: 'capitalize', fontFamily: 'Roboto-Slab', fontSize: '12px', lineHeight: '1px' }}>WEB DEVELOPMENT</Nav.Link>
-          <Nav.Link href="/my-portfolio/uxdesign" id='ux-link' style={{marginRight: '60px', color: '#F2CA52', fontWeight: 'bold', textTransform: 'capitalize', fontFamily: 'Roboto-Slab', fontSize: '12px', lineHeight: '1px' }}>UX DESIGN</Nav.Link>
-          <Nav.Link href="/my-portfolio/writing" id='writing-link' style={{marginRight: '60px', color: '#F2CA52', fontWeight: 'bold', textTransform: 'capitalize', fontFamily: 'Roboto-Slab', fontSize: '12px', lineHeight: '1px' }}>WRITING</Nav.Link>
-          <Nav.Link href="/my-portfolio/contact" id='contact-link' style={{marginRight: '60px', color: '#F2CA52', fontWeight: 'bold', textTransform: 'capitalize', fontFamily: 'Roboto-Slab', fontSize: '12px', lineHeight: '1px' }}>CONTACT</Nav.Link>
+          <Nav.Link href="/" id='home-link' style={{ marginLeft: '400px', marginRight: '30px', color: '#000', fontWeight: '400', textTransform: 'lowercase', fontFamily: 'Roboto-Slab', fontSize: '20px', lineHeight: '2px', marginTop:'10px' }}>HOME</Nav.Link>
+          <Nav.Link href="/my-portfolio/about" id='about-link' style={{marginRight: '30px', color: '#000', fontWeight: '400', textTransform: 'lowercase', fontFamily: 'Roboto-Slab', fontSize: '20px', lineHeight: '2px', marginTop:'10px' }}>ABOUT</Nav.Link>
+          <Nav.Link href="/my-portfolio/webdevelopment" id='webdev-link' style={{marginRight: '30px', color: '#000', fontWeight: '400', textTransform: 'lowercase', fontFamily: 'Roboto-Slab', fontSize: '20px', lineHeight: '2px', marginTop:'10px' }}>WEB DEVELOPMENT</Nav.Link>
+          <Nav.Link href="/my-portfolio/uxdesign" id='ux-link' style={{marginRight: '30px', color: '#000', fontWeight: '400', textTransform: 'lowercase', fontFamily: 'Roboto-Slab', fontSize: '20px', lineHeight: '2px', marginTop:'10px' }}>UX DESIGN</Nav.Link>
+          <Nav.Link href="/my-portfolio/writing" id='writing-link' style={{marginRight: '30px', color: '#000', fontWeight: '400', textTransform: 'lowercase', fontFamily: 'Roboto-Slab', fontSize: '20px', lineHeight: '2px', marginTop:'10px' }}>WRITING</Nav.Link>
+          <Nav.Link href="/my-portfolio/contact" id='contact-link' style={{marginRight: '30px', color: '#000', fontWeight: '400', textTransform: 'lowercase', fontFamily: 'Roboto-Slab', fontSize: '20px', lineHeight: '2px', marginTop:'10px' }}>CONTACT</Nav.Link>
   
           
 
