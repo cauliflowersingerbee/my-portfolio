97926520ee1a1e97d68246dfa0fb15ac6ecac293
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-20 19:26:58 +0100

    updates

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from '../navbar/navbar';
 import './about.scss';
-import LogoImage from '../imgs/portfolio-logo.svg';
 import AboutImage from '../imgs/about-img.svg'
 import AboutVector from '../imgs/about-vector.svg'
 
@@ -19,8 +18,8 @@ export default class AboutView extends React.Component {
       <img id='about-vector' src={AboutVector} alt=''/>
       <div id='about-me'>About Me</div>
         <div id='about-text'>
-        I’m Okwiri, a writer, web developer and UX Designer. After years of working as a fiction writer, I'm venturing into tech. I am skilled in JavaScript, Node.js, React, React Native, and Angular. I am also skilled in UX research, wireframing, prototyping, and usability testing.  
-Designing and creating stories is an important part of my writing work, something I approach with great care and thoughtfulness. I have found surprising similarities between story design and software design. The empathy and detail with which I craft my fiction characters, I now bring to my personas in the UX design process. The structure of narrative and the meticulousness of grammar, I now bring to the structure and syntax of code. I am interested in building software that is human-centered, aesthetic, and useful.
+        I am a fiction writer who, after years of interest in programming, finally scratched the itch and ventured into tech. I now write both stories and code. I like designing user experience too. I am skilled in JavaScript, Node.js, React, React Native, and Angular. I am also skilled in UX research, wireframing, prototyping, and usability testing.  
+Designing and creating stories is an important part of my writing work, something I approach with great care and thoughtfulness. I have found surprising similarities between story design and software design. The empathy and detail with which I craft my fiction characters, I now bring to my personas in the UX design process. The structure of narrative and the meticulousness of grammar, I now bring to the structure and syntax of code. I am interested in building software that is human-centered, aesthetic, and useful. 
 
         </div>
       </div>
