d3a1c77fc2af8ae64167dcfe6a501006582932d1
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-21 17:01:18 +0100

    imported carousel template

--- src/components/webdev/webdev.jsx
+++ src/components/webdev/webdev.jsx
@@ -10,7 +10,7 @@ import AptlyImg from '../imgs/aptly-img.svg';
 import TitbitImg from '../imgs/titbit-img.svg';
 import PokedexImg from '../imgs/pokedex-img.svg';
 import UserFlows from '../imgs/user-flows.svg';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Carousel } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -35,16 +35,44 @@ export default class WebDevView extends React.Component {
     <div className='container'>
       <div id='navbar'>
         <Navbar/>
-      <div>
-
-      
       </div>
-       
+      <Carousel fade>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
 
-      </div>
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
 
-
-
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
 
       </div>
             
