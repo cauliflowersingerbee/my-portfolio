5e5f4b7245492d520b87bf7a7f7163cc8cbd0968
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-21 12:13:12 +0100

    updates

--- src/components/writing/writing.jsx
+++ src/components/writing/writing.jsx
@@ -4,10 +4,6 @@ import LogoImage from '../imgs/portfolio-logo.svg';
 import './writing.scss'
 import WorkImage from '../imgs/work-img.svg';
 import WorkVector from '../imgs/work-vector.svg';
-import SquigglyLine from '../imgs/squiggly-line.svg';
-import SquigglyLineThree from '../imgs/squiggly-line.svg';
-
-//import { Router } from 'react-router-dom';
 
 
 
@@ -20,10 +16,8 @@ export default class WritingView extends React.Component {
       return (
       <div>
       <Navbar/>
-      <img id='logo-img' src={LogoImage} alt=''/>
       <img id='work-vector' src={WorkVector} alt=''/>
       <img id='work-img' src={WorkImage} alt=''/>
-      <img id='squiggly-line-writing' src={SquigglyLine} alt=''/>
       
       
       <p id='work-text'>My author page is still under construction. Please check back soon!</p>
