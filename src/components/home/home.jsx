b8b192b8244336986345e5f83af23c5ed4ad051a
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-06-01 09:32:38 +0200

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,6 +11,7 @@ import './home.scss';
 export default class HomeView extends React.Component {  
     
     render() {
+      console.log('Welcome!')
     
       return (
       <div className='container'>
