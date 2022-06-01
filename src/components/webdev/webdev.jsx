d683e29cf7f8c3fc66797f1b01cb4ae528a7fcda
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-06-01 09:52:01 +0200

    updated portfolio with new project

--- src/components/webdev/webdev.jsx
+++ src/components/webdev/webdev.jsx
@@ -8,6 +8,7 @@ import MyFlixReactImg from '../imgs/myflix-react-img.svg';
 import MyFlixApiImg from '../imgs/myflix-api-img.svg';
 import AptlyImg from '../imgs/aptly-img.svg';
 import TitbitImg from '../imgs/titbit-img.svg';
+import SpaceyImg from '../imgs/spacey-exxe-img.png';
 import PokedexImg from '../imgs/pokedex-img.svg';
 import { Card, Row, Col } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
@@ -58,6 +59,39 @@ export default class WebDevView extends React.Component {
 
       <div id='myflix-angular' >
 
+<div>
+
+</div>
+<div className='spacey-exxe' style={{border: 'solid 0.5px #000', padding:'1rem'}} >
+
+                    <p className='title' style={{marginTop: '0.5rem'}}>Spacey Exxe</p>
+                    <img alt='screenshot of live Spacey Exxe project' src={SpaceyImg} id='webdev-img-angular'/>
+                    <p className='paragraph'>This is a single-page, responsive movie app built with Angular, with routing and several
+                    interface views. The client-side is an Angular rendition of a previous project that I built 
+                    with React. It is designed using Angular Material, with documentation created using Typedoc and JSDoc. </p>
+
+                  
+ <Row>
+            <Col id='site-links' ><Card.Link href="https://github.com/cauliflowersingerbee/myFlix-Angular-client" target="_blank">Github</Card.Link></Col>
+            <Col id='site-links' ><Card.Link href="https://cauliflowersingerbee.github.io/myFlix-Angular-client/welcome" target="_blank">Live Site</Card.Link></Col>
+            <Col></Col>
+           <Col></Col>
+           <Col></Col>
+           <Col></Col>
+           <Col></Col>
+         
+        </Row>
+</div>
+
+
+
+</div>
+
+
+
+
+      <div id='myflix-angular' >
+
         <div>
         
         </div>
@@ -87,38 +121,7 @@ export default class WebDevView extends React.Component {
         </div>
 
 
-         <div id='myflix-react' >
-
-        <div>
         
-        </div>
-        <div className='webdev-text' style={{border: 'solid 0.5px #000', padding:'1rem'}}>
-          
-                            <p className='title' style={{marginTop: '0.5rem'}}>KINO NOIR REACT</p>
-                            <img alt='screenshot of live React project' src={MyFlixReactImg} id='webdev-img-react'/>
-                            <p className='paragraph'>This is the client-side of a movie API. It's built using the MERN tech stack 
-                                (MongoDB, Express, React, and Node.js). It’s a single-page application (SPA). 
-                                It uses routing to navigate between views and share URLs. It uses Parcel as its build tool.
-                                The user interface is made up of a number of views e.g. the main view, single movie view,
-                                login view, registration view and profile view. </p>
-
-                          
-                                <Row>
-                    <Col id='site-links' ><Card.Link href="https://github.com/cauliflowersingerbee/myFlix-Angular-client" target="_blank">Github</Card.Link></Col>
-                    <Col id='site-links' ><Card.Link href="https://cauliflowersingerbee.github.io/myFlix-Angular-client/welcome" target="_blank">Live Site</Card.Link></Col>
-                     <Col></Col>
-                   <Col></Col>
-                   <Col></Col>
-                   <Col></Col>
-                   <Col></Col>
-                 
-                </Row>
-        </div>
-        
-
-       
-        </div>
-
 
 
         <div id='myflix-react' >
